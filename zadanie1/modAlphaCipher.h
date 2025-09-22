3f0a6e37105e5682b6a710e85d5fdbe33b4c30cb
mezin24pt1 <andrejmezin713@gmail.com>
2025-09-23 01:32:28 +0300

    Update modAlphaCipher.h

---
+++ zadanie1/modAlphaCipher.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <locale>
+#include <codecvt>
+using namespace std;
+
+class modAlphaCipher
+{
+private:
+    wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; 
+    map<wchar_t, int> alphaNum; 
+    vector<int> key; 
+    vector<int> convert(const wstring& s); 
+    wstring convert(const vector<int>& v); 
+public:
+    modAlphaCipher() = delete; 
+    modAlphaCipher(const wstring& skey);
+    wstring encrypt(const wstring& open_text); 
+    wstring decrypt(const wstring& cipher_text); 
+};
